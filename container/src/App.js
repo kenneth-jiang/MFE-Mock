c56783e636f178594c54a0706504d7ea1ec115a1
kenneth-jiang <kennethjiang1024@gmail.com>
2022-05-09 14:57:09 -0400

    MFconfig change.

--- container/src/App.js
+++ container/src/App.js
@@ -1,20 +1,24 @@
-import React, { Suspense } from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HeaderComponent from "./components/HeaderComponent";
-import InputComponent from "./components/InputComponent";
-import AboutComponent from "./components/AboutComponent";
-import LocalizationComponent from "./components/LocalizationComponent";
+const HeaderLazy = lazy(() => import("./components/HeaderComponent"));
+const InputLazy = lazy(() => import("./components/InputComponent"));
+const AboutLazy = lazy(() => import("./components/AboutComponent"));
+const LocalizationLazy = lazy(() => import("./components/LocalizationComponent"));
+// import HeaderComponent from "./components/HeaderComponent";
+// import InputComponent from "./components/InputComponent";
+// import AboutComponent from "./components/AboutComponent";
+// import LocalizationComponent from "./components/LocalizationComponent";
 import CustomRouter from "./CustomRouter";
 
 export default () => {
 	return (
 		<BrowserRouter>
-			<HeaderComponent />
+			<HeaderLazy />
 			<Suspense fallback={"Loading..."}>
 				<Routes>
-					<Route path="/about" element={<AboutComponent />} />
-					<Route path="/990-finder" element={<InputComponent />} />
-					<Route path="/localization" element={<LocalizationComponent />} />
+					<Route path="/990-finder" element={<InputLazy />} />
+					<Route path="/about" element={<AboutLazy />} />
+					<Route path="/localization" element={<LocalizationLazy />} />
 					<Route path="/" element={<div>Hello World! This page should be rendered when the Candid logo is clicked.</div>} />
 				</Routes>
 			</Suspense>
