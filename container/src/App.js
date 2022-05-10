9feab2dccd775a2b42c1d9cb176fbfa12fb11f5b
kenneth-jiang <kennethjiang1024@gmail.com>
2022-05-09 21:59:47 -0400

    Trying again.

--- container/src/App.js
+++ container/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 const HeaderLazy = lazy(() => import("./components/HeaderComponent"));
 const InputLazy = lazy(() => import("./components/InputComponent"));
 const AboutLazy = lazy(() => import("./components/AboutComponent"));
-// const LocalizationLazy = lazy(() => import("./components/LocalizationComponent"));
+const LocalizationLazy = lazy(() => import("./components/LocalizationComponent"));
 // import HeaderComponent from "./components/HeaderComponent";
 // import InputComponent from "./components/InputComponent";
 // import AboutComponent from "./components/AboutComponent";
@@ -18,7 +18,7 @@ export default () => {
 				<Routes>
 					<Route path="/990-finder" element={<InputLazy />} />
 					<Route path="/about" element={<AboutLazy />} />
-					{/* <Route path="/localization" element={<LocalizationLazy />} /> */}
+					<Route path="/localization" element={<LocalizationLazy />} />
 					<Route path="/" element={<div>Hello World! This page should be rendered when the Candid logo is clicked.</div>} />
 				</Routes>
 			</Suspense>
